428437dec6c091eac13a53d3f5e1b75d27bdcaa1
Tetiana <cat.tanyusha0122@gmail.com>
2024-11-19 17:56:03 +0200

    add styles for desktop and modal

--- js/index.js
+++ js/index.js
@@ -1,18 +1,31 @@
 window.addEventListener('load', function(){
-    const swiper = new Swiper('.swiper', {
-        loop: true,
-        slidesPerView: 3,
-        spaceBetween: 20,
-      
-        pagination: {
-          el: '.formats-pagination',
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.arrow-next',
-          prevEl: '.arrow-prev',
-        },
-      
-    });
+
+  const toggleModalBtn = document.querySelectorAll('.header-btn, .modal-overflow, .modal-close');
+  const modal = document.querySelector('.modal');
+  const modalOveflow = document.querySelector('.modal-overflow');
+
+  if (toggleModalBtn){
+    toggleModalBtn.forEach(btn=>{
+      btn.addEventListener('click', function(){
+        modal.classList.toggle('is-hidden');
+      })
+    })
+  }
+
+  const swiper = new Swiper('.swiper', {
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 20,
+    
+      pagination: {
+        el: '.formats-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.arrow-next',
+        prevEl: '.arrow-prev',
+      },
+    
+  });
 });
